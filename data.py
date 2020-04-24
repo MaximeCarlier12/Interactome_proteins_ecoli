572b368ed505a26cb1bd48a00a60191163cc826a
Maxime Carlier <Maxime.carlier@insa-lyon.fr>
2020-04-24 21:50:12 +0200

    Get control and protein replicates and intersection of replicates
    Interesection in not perfect because different databses used

--- data.py
+++ data.py
@@ -9,6 +9,7 @@ import glob
 os.chdir("/home/carlier/Documents/Stage/Interactome_proteins_ecoli/")
 
 CONDITION = ['LB log', 'LB O/N' ,'M9 0.2% ac O/N']
+CONTROL = ['MG1655 (TYPE A)', 'MG1655 (placI)mVenus-SPA-pUC19 (TYPE C2)', 'MG1655']
 PROTEIN = ['DnaA', 'DiaA', 'Hda', 'SeqA', 'HolD', 'DnaB', 'DnaG', 'NrdB']
 
 def header(msg):
@@ -24,6 +25,8 @@ def load_df(bfNumber):
     full_path = glob.glob(path_batch+"batch "+bfNumber[0]+"/*"+bfNumber[0]+bfNumber[1]+".reportbuilder.csv")
   df = pd.read_csv(full_path[0], sep=',', header=23)
 # We remove the first lines because they are not part of data. 
+  if not "Accession" in df.columns: # some files have one additional row in the description part. 
+    df = pd.read_csv(full_path[0], sep=',', header=24)
   df = df.set_index("Accession")
   #header("df.head")
   #print(df.head) #check it is correct. 
@@ -38,7 +41,7 @@ def load_sample_code():
 
 def load_based_screen_controls(): 
   '''get controls part of based screen in MS xlsx file.
-Returns a list of pandas containing : the resin unspecific control, the tag unspecific control and the untagged whole cell lysates'''
+Returns a panda containing 3 rows : the resin unspecific control, the tag unspecific control and the untagged whole cell lysates'''
 
   df = pd.read_excel("MS PPI screen - sample coding, 200226.xlsx", sheet_name = 1, header = 2, skipfooter = 44-13)
   df = df.rename(columns = {'LB log.1':'repeat LB log','LB O/N.1' : 'repeat LB O/N', 'M9 0.2% ac O/N.1' : 'repeat M9 0.2% ac O/N', 'strain - tagged protein (all in MG1655 genetic background)':'protein'}) # rename repeat number for each condition. 
@@ -46,39 +49,67 @@ Returns a list of pandas containing : the resin unspecific control, the tag unsp
   typeResin = df.loc[df['strain'] == 'MG1655 (TYPE A)']
   typeTag = df.loc[df['strain'] == 'MG1655 (placI)mVenus-SPA-pUC19 (TYPE C2)']
   typeAbundant = df.loc[df['strain'] == 'MG1655']
-  return [typeResin, typeTag, typeAbundant]
+  return pd.concat([typeResin, typeTag, typeAbundant])
 
 def load_based_screen_samples(): 
   '''get samples part of based screen in MS xlsx file'''
 
   df = pd.read_excel("MS PPI screen - sample coding, 200226.xlsx", sheet_name = 1, header = 16)
-  header("index")
   df = df.rename(columns = {'LB log.1':'repeat LB log','LB O/N.1' : 'repeat LB O/N', 'M9 0.2% ac O/N.1' : 'repeat M9 0.2% ac O/N', 'strain - tagged protein (all in MG1655 genetic background)':'protein'}) # rename repeat number for each condition. 
   return df
 
-def get_replicates(MS, protein, condition):
-  ''' From one protein and one condition, it gets all replicates dataframes.'''
-
-  prot_row = MS[MS.protein=='DnaA']
-  print(prot_row.dtypes)
-  repeat_condition = 'repeat '+condition
-  if prot_row[repeat_condition].iloc[0] == 3:
-    batches = prot_row[condition].iloc[0]  # get batches of the different replicates.
-    batches = batches.replace(';',',') # replace ';' by ','
-    batches = batches.replace(" ", "") # remove spaces
-    batches = batches.split(',') # separate the replicates
-    header('batches')    
-    print(batches)
-    return [load_df(batches[0]), load_df(batches[1]), load_df(batches[2])]
-  else :
-    print("Not enough replicates")
+def get_control_replicates(pd_control, type_control, condition):
+  ''' From the pd.control file, get pd files of the different replicates available for a certain type of control and a certain type of condition.'''
+  if not condition in CONDITION : 
+    print("Error condition")
     return None
+  if not type_control in CONTROL : 
+    print("Error control")
+    return None
+  control_row = pd_control[pd_control.strain== type_control] # get only one row.
+  #repeat_condition = 'repeat '+condition
+  batches = control_row[condition].iloc[0]  # get batches of the different replicates.
+  batches = batches.replace(';',',') # replace ';' by ',' because there are two separator ways in the initial file. 
+  batches = batches.replace(" ", "") # remove spaces
+  batches = batches.split(',') # separate the replicates
+  header('batches')    
+  print(batches)
+  return [load_df(batches[i]) for i in range(len(batches))]
+
+def get_protein_replicates(MS, protein, condition):
+  ''' MS is the sample file. From one protein and one condition, it gets all replicates dataframes.'''
+  if not condition in CONDITION : 
+    print("Error condition")
+    return None
+  if not protein in PROTEIN : 
+    print("Error protein")
+    return None
+  prot_row = MS[MS.protein== protein] # get only one row.
+  repeat_condition = 'repeat '+condition
+  if prot_row[repeat_condition].iloc[0] != 3:
+    print("Be carreful, there are not 3 replicates")
+  batches = prot_row[condition].iloc[0]  # get batches of the different replicates.
+  batches = batches.replace(';',',') # replace ';' by ',' because there are two separator ways in the initial file. 
+  batches = batches.replace(" ", "") # remove spaces
+  batches = batches.split(',') # separate the replicates
+  header('batches')    
+  print(batches)
+  return [load_df(batches[i]) for i in range(len(batches))]
+
+def intersect_index(replicates):
+  ''' From a list of replicates, gives a list of accession number proteins present in all replicates.'''
+  intersect = replicates[0].index
+  for i in replicates : 
+    intersect = intersect.intersection(i.index)
+  return intersect
 
 #df = load_df("A1")
 #load_sample_code()
-controls = load_based_screen_controls()
-samples = load_based_screen_samples()
-#replicates = get_replicates(MS, 'DnaA', 'M9 0.2% ac O/N')
-#print(replicates)
-
+all_controls = load_based_screen_controls()
+one_control = get_control_replicates(all_controls, 'MG1655','LB log')
+all_samples = load_based_screen_samples()
+[rep1,rep2,rep3] = get_protein_replicates(all_samples, 'DnaB', 'M9 0.2% ac O/N')
+intersect = intersect_index([rep2,rep3])
+header('intercept')
+print("rep3",len(rep3.index), "rep2", len(rep2.index), "intersect", len(intersect))
 
